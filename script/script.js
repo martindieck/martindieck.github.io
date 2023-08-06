18caac1575c5412436372518d09f5b3e78c05c49
martindieck <krutterlax@gmail.com>
2023-08-06 18:19:11 -0500

    Updated Webpage for 2023

--- script/script.js
+++ script/script.js
@@ -1,18 +1,3 @@
-//Script Document
-
-/*/ add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
-
-window.onscroll = function() {
-    var top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
-    }
-}*/
-
 function navBar() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
